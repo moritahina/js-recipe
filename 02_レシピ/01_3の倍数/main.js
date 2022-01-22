1c459d46a43cc562ca8ef3b0957942093c1495d4
moritahina <hinamorita0315@yahoo.co.jp>
2022-01-23 00:01:35 +0900

    [3の倍数]を学習しました。

---
+++ 02_レシピ/01_3の倍数/main.js
@@ -0,0 +1,25 @@
+const number = 24
+
+if (number % 3 === 0) {
+  console.log(number + "!!!!!!!")
+} else {
+  console.log(number)
+}
+for (let n = 1; n <= number; n++) {
+  if (n % 3 === 0) {
+    console.log(n + "!!!!!!!")
+  } else {
+    console.log(n)
+  }
+}
+const genkiFunction = function (number) {
+  // n を 1 から、 number まで、 値を 1 ずつ増やしながら、 for の {} の中身を繰り返す
+  for (let n = 1; n <= number; n++) {
+    if (n % 3 === 0) {
+      console.log(`${n}!!!!!!!`)
+    } else {
+      console.log(n)
+    }
+  }
+}
+genkiFunction(100)
