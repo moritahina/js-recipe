beb7c45348a1e4fa6f8b0b06159f4c8d71bcb163
moritahina <hinamorita0315@yahoo.co.jp>
2022-01-23 10:55:01 +0900

    [□と○]を学習しました。

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,6 +1,7 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
 // ボタンをクリックしたときの処理を登録
+let count = 0
 plusButton.onclick = function () {
   // ここにクリック後の処理を書く
   count += 1
