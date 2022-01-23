54bb51dbd4bae2624f19dc8bea2151fe9d1e807c
moritahina <hinamorita0315@yahoo.co.jp>
2022-01-23 14:19:19 +0900

    [Trello (保存機能なし)]を学習しました。

--- 02_レシピ/01_ストップウォッチ/main.js
+++ 02_レシピ/01_ストップウォッチ/main.js
@@ -1,28 +1,20 @@
-//const display = document.getElementById("display")
-//const button = document.getElementById("button")
-//let count = 0
-//const countUp = funation() {
-//    count += 1
-//    display.textContent = count / 100
-//}
-//button.onclick = function() {
-//setInterval(countUp, 10)
-//button.textContent = "stop"
-//}
 const display = document.getElementById("display")
 const button = document.getElementById("button")
-
 let count = 0
-
 const countUp = function () {
   // count を更新
   count += 1
   // count を秒単位にして表示
   display.textContent = count / 100
 }
-
+let id = null
 button.onclick = function () {
-  // 10ms ごとに countUp を実行するように登録する
-  setInterval(countUp, 10)
-  button.textContent = "stop"
+  if (id === null) {
+    id = setInterval(countUp, 10)
+    button.textContent = "stop"
+  } else {
+    clearInterval(id)
+    id = null
+    button.textContent = "start"
+  }
 }
---
+++ 02_レシピ/02_Trello（保存機能なし）/main.js
@@ -0,0 +1,32 @@
+const inputElement = document.grtElementById("input-todo")
+const container = document.getElementById("cards-container")
+const addButton = document.getElementById("add-button")
+//追加ボタンを押したとき
+addButton.onlicl = function () {
+  //カードを作成する
+  const card = createCard(inputElement.value)
+  //valueって何だ？
+  container.append(card)
+  //cardをcontainerの中に移動する
+  //入力欄を空にする
+  inputElement.value = ""
+}
+const createCard = function (text) {
+  //inputElement.valueが分からない
+  const card = document.createElement("div")
+  card.className = "card"
+  const todo = document.createElement("div")
+  todo.className = "todo"
+  todo.textContent = text
+  card.append(todo)
+  container.append(card)
+  inputElement.value = ""
+  //削除ボタンを作る
+  const deleteButton = document.createElement("div")
+  deleteButton.className = "delete"
+  deleteButton.onclick = function () {
+    card.remove()
+  }
+  card.append(deleteButton)
+  return card
+}
